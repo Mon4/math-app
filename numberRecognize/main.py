9aee25161af390fffb2af3e9be770385829497d5
Monika Etrych <44522588+Mon4@users.noreply.github.com>
2023-03-09 09:37:16 +0100

    confusion_matrix

--- numberRecognize/main.py
+++ numberRecognize/main.py
@@ -1,7 +1,10 @@
+import numpy as np
 import tensorflow as tf
-import gradio as gr
 import keras
 from matplotlib import pyplot as plt
+from tensorflow.python.ops.confusion_matrix import confusion_matrix
+import seaborn as sn
+
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 # normalization color values
@@ -20,7 +23,39 @@ history = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=1
 model.save("myModel")
 
 
+# statistics
 scores = model.evaluate(x_test, y_test, verbose=0)
+predicted = model.predict(x_test)
+predicted = np.argmax(predicted, axis=1)
+
+cm = confusion_matrix(y_test, predicted)
+sn.heatmap(cm, cmap='Blues', annot=True, fmt='g')
+plt.title(label="Macierz błędu")
+plt.show()
+
+# FP = confusion_matrix.sum(axis=0) - np.diag(confusion_matrix)
+# FN = confusion_matrix.sum(axis=1) - np.diag(confusion_matrix)
+# TP = np.diag(confusion_matrix)
+# TN = confusion_matrix.values.sum() - (FP + FN + TP)
+#
+# # Sensitivity, hit rate, recall, or true positive rate
+# TPR = TP/(TP+FN)
+# # Specificity or true negative rate
+# TNR = TN/(TN+FP)
+# # Precision or positive predictive value
+# PPV = TP/(TP+FP)
+# # Negative predictive value
+# NPV = TN/(TN+FN)
+# # Fall out or false positive rate
+# FPR = FP/(FP+TN)
+# # False negative rate
+# FNR = FN/(TP+FN)
+# # False discovery rate
+# FDR = FP/(TP+FP)
+#
+# # Overall accuracy
+# ACC = (TP+TN)/(TP+FP+FN+TN)
+
 print("Baseline Error: %.2f%%" % (100-scores[1]*100))
 
 
@@ -29,10 +64,10 @@ def classify(input):
     return {str(i): prediction[i] for i in range(10)}
 
 
-# gradio website
-label = gr.outputs.Label(num_top_classes=3)
-interface = gr.Interface(fn=classify, inputs="sketchpad", outputs=label, live=True)
-interface.launch()
+# # gradio website
+# label = gr.outputs.Label(num_top_classes=3)
+# interface = gr.Interface(fn=classify, inputs="sketchpad", outputs=label, live=True)
+# interface.launch()
 
 plt.subplot(1, 2, 1)
 plt.plot(history.history['accuracy'])
@@ -50,4 +85,5 @@ plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper right')
 plt.show()
+
 
